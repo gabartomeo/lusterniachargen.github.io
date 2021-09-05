286e09cd3d017333f658f211850e101fe8ac6494
Cory Roush <coryroush@gmail.com>
2021-09-05 18:12:37 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '2PACX-1vQ8Y2oWA7u8Q_hWwFIOSqc6pL9y3ugAyDFrrZsy3Qbs80F6B0V0L46valDN1yPooTDH6JCAd4U1E-bw' );
+    WTF.init( 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8Y2oWA7u8Q_hWwFIOSqc6pL9y3ugAyDFrrZsy3Qbs80F6B0V0L46valDN1yPooTDH6JCAd4U1E-bw/pubhtml' );
 
 });
