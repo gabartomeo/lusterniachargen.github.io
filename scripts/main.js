47615f31797840aa369ec4e2a114061e77d9b03d
Cory Roush <coryroush@gmail.com>
2021-09-05 18:20:53 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,39 +3,39 @@ $(function() {
 
     // Populate using an object literal
 
-    // WTF.init({
+     WTF.init({
 
-    //     heading: [
-    //         "It's a fucking",
-    //         "Check this shit out, a fucking"
-    //     ],
-    //     response: [
-    //         "Already fucking seen one",
-    //         "Give me a-fucking-nother one",
-    //         "Don't really give a fuck"
-    //     ],
-    //     template: [
-    //         "Big @color @animal",
-    //         "Silly @animal with @color fur"
-    //     ],
-    //     animal: [
-    //         "dog",
-    //         "cat",
-    //         "rabbit"
-    //     ],
-    //     color: [
-    //         "red",
-    //         "blue",
-    //         "green",
-    //         "yellow"
-    //     ]
-    // });
+         heading: [
+             "It's a fucking",
+             "Check this shit out, a fucking"
+         ],
+         response: [
+             "Already fucking seen one",
+             "Give me a-fucking-nother one",
+             "Don't really give a fuck"
+         ],
+         template: [
+             "Big @color @animal",
+             "Silly @animal with @color fur"
+         ],
+         animal: [
+             "dog",
+             "cat",
+             "rabbit"
+         ],
+         color: [
+             "red",
+             "blue",
+             "green",
+             "yellow"
+         ]
+     });
 
     // Populate using a JSON file
     // WTF.init( 'sample.json' );
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '1wAYn1oDBtV2U2OfBNa9uf3mkSBCYJv21k6g4sO_phhg' );
+    // WTF.init( '1wAYn1oDBtV2U2OfBNa9uf3mkSBCYJv21k6g4sO_phhg' );
 
 });
