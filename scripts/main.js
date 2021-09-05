7159c86c9b6dad06a78869b873e72b404b039241
Cory Roush <coryroush@gmail.com>
2021-09-05 19:16:24 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ $(function() {
              "Don't really give a daka about this, one more."
          ],
          template: [
-             "@adjective @gender @race @class from @faction, with a talent for @talent.",
+             "@adjective @gender @race @class from @faction, with a talent for @talent and aspirations to @aspiration",
          ],
          class: [
              "B.E.A.S.T.",
@@ -64,6 +64,13 @@ $(function() {
              "genius",
              "lazy"
          ],
+        aspiration: [
+             "become a famed Bounty Hunter",
+             "become an incompetent Bounty Hunter",
+             "lead their faction as a benevolent dictator",
+             "spread their hatred for the Fatar",
+             "have a world-famous band"
+        ]
      });
 
     // Populate using a JSON file
