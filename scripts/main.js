1d4e66e2645d5b4b3993ed9eaeeaaf699cdb8c54
Cory Roush <coryroush@gmail.com>
2022-04-05 12:19:27 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ $(function() {
              "Need another idea? Try again!"
          ],
          template: [
-             "@adjective and @adjective @gender @race @class_spirit from @faction_spirit, with a talent for @talent and aspirations to @aspiration when they grow up. They follow the tenets of @god_spirit."
+             "@adjective and @adjective @gender @race @class_spirit from @faction_spirit, with a talent for @talent and aspirations to @aspiration when they grow up. They follow the tenets of @god_spirit.",
              "@adjective and @adjective @gender @race @class_shadow from @faction_shadow, with a talent for @talent and aspirations to @aspiration when they grow up. They follow the tenets of @god_shadow.",
              "@gender @race @class_spirit in @faction_spirit. They are @adjective and @adjective, with a penchant for @talent and the desire to @aspiration someday. Though not overly religious, they follow @god_spirit.",
              "@gender @race @class_shadow in @faction_shadow. They are @adjective and @adjective, with a penchant for @talent and the desire to @aspiration someday. Though not overly religious, they follow @god_shadow.",
